614b4ab361ee85fd9a582888bdb4eca044549d77
Voravit Euavatanakorn <voravite@yahoo.com>
2018-03-07 09:07:13 +0700

    check

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 var bot = linebot({
     channelId: process.env.CHANNEL_ID,
     channelSerect: process.env.CHANNEL_SECRET,
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    verify: true
 });
 
 bot.on('message', function (event) {
