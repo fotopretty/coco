6c5cf19efc0cbd2258b0453dac6811f9e8e19da0
Voravit Euavatanakorn <voravite@yahoo.com>
2018-03-07 08:33:22 +0700

    console

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ var linebot = require('linebot');
 
 require('dotenv').config();
 
-var bot  = linebot({
+var bot = linebot({
     channelId: process.env.CHANNEL_ID,
     channelSerect: process.env.CHANNEL_SECRET,
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
     verify: true
 });
 
-bot.on('message', function(event){
-    return event.reply(event.message.text).then(function(data){
-        console.log('Success', data)
-    }).catch(function(error){
-        console.log('Error', error);
+bot.on('message', function (event) {
+    event.reply(event.message.text).then(function (data) {
+        console.log('Success', data);
     });
+    console.log('what');
 });
 
-bot.listen('/webhook', process.env.PORT, function(){
+bot.listen('/webhook', process.env.PORT, function () {
     console.log('LineBot is Running');
 });
