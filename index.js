db02fdb19de68a4534787dfb35ee439cd04ca40a
Voravit Euavatanakorn <voravite@yahoo.com>
2018-03-07 09:49:39 +0700

    check

--- index.js
+++ index.js
@@ -2,34 +2,12 @@ var linebot = require('linebot');
 const { LineClient } = require('messaging-api-line');
 
 require('dotenv').config();
-const client = Lineclient.connect(process.env.CHANNEL_ACCESS_TOKEN, process.env.CHANNEL_SECRET);
-/*
-
-var bot = linebot({
-    channelId: process.env.CHANNEL_ID,
-    channelSerect: process.env.CHANNEL_SECRET,
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-    verify: true
-});
-
-bot.on('message', function (event) {
-    event.reply(event.message.text).then(function(data){
-        console.log('Success' , data);
-    }).catch(function(error){
-        console.log('Error', error);
-    });
-});
-
-bot.listen('/webhook', process.env.PORT || 80, function () {
-    console.log('LineBot is Running');
-});
-*/
-
+const client = LineClient.connect(process.env.CHANNEL_ACCESS_TOKEN, process.env.CHANNEL_SECRET);
 client.reply(REPLY_TOKEN, [
     {
         type: 'text',
-        text: 'Hello'
-    }
+        text: 'Hello',
+    },
 ]);
 
 client.listen('/webhook', process.env.PORT || 80, function(){
